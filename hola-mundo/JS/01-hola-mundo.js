0db2ccffebfb630b66fe180c33dc4dd52ca41d93
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-04-28 08:52:03 +0200

    cuarto commit

---
+++ hola-mundo/JS/01-hola-mundo.js
@@ -0,0 +1,5 @@
+alert("Hola mundo en JavaScript");
+document.write("Hola mundo desde fichero externo");
+console.log("Muestra en consola.");
+console.log(84521*23);
+
---
+++ hola-mundo/JS/02-variables.js
@@ -0,0 +1,7 @@
+//VARIABLES
+//Una variable es un contenedor de información
+
+var pais = "España";
+var continente = "Europa";
+
+console.log(pais,continente);
