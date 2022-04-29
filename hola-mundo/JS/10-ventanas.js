80465e5bafeff9334cae1c2d556ea4708b33f4d1
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-04-29 17:10:35 +0200

    commit de inputs y popups

---
+++ hola-mundo/JS/10-ventanas.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//alerta
+alert("popup, rollo error o similar.");
+
+//confirmación
+//si lo metemos en variable el confirm nos muestra si se ha aceptado o cancelado.
+var confirmacion= confirm("confirmación, sale boton para aceptar o cancelar.");
+console.log(confirmacion);
+
+//ingreso de datos
+//si lo metemos en variable el confirm nos muestra si se ha aceptado o cancelado.
+//siempre sera string lo introducido.
+var resultadoIngreso =prompt("aqui sale la pregunta con popup,y el valor de al lado separado por coma, que es el valor por defecto.",25);
+console.log(resultadoIngreso);
