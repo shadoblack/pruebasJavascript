efe119f168111ec3eb138e253a9365a739cf2762
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-04-29 15:46:29 +0200

    commit de bucles

---
+++ hola-mundo/JS/08-bucle-for.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//bucle FOR
+//Bucle es una estructura de control que se repite varias veces.
+
+var numeros = 100;
+
+for(var i =0;i<=numeros;i++){
+    console.log(i);
+
+    debugger;
+}
