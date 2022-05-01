f7e2702c4195dcb366ac45e8833a6e21e38fe997
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-02 00:16:36 +0200

    commit con array, se puede optimizar y hacer sin array

---
+++ hola-mundo/JS/13-ejercicio3.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//Hacer un programa que muestre todos los numeros entre 2 numeros introducidos por el usuario
+
+var numero1 = parseInt(prompt('introduce un numero',0));
+var numero2 = parseInt(prompt('introduce otro numero',0));
+var cadena =[];
+for(var i=numero1;i<=numero2;i++){
+    cadena.push(i);
+    
+    
+    
+}
+
+alert('los numeros que hay entre ' + numero1 + ' y ' + numero2 + ' son ' + cadena);
