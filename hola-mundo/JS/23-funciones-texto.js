5f12de7ea3d971d83876d6c16a5f94a10ef98d97
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-03 00:14:45 +0200

    funciones texto

--- hola-mundo/JS/23-funciones-texto.js
+++ hola-mundo/JS/23-funciones-texto.js
@@ -38,4 +38,8 @@ var busqueda = texto1.lastIndexOf("Hola"); //se puede usar tambien .search
 // con .startsWith("palabra que buscamos") devolvera si es true o false si tiene el contenido, y siempre al principio del string.
 // con .endsWith() lo mismo pero por donde termina
 // con .includes() devuelve true o false si encuentra o no esto en este string
+// con .replace(lo que voy a remplazar,loremplazado)
+// con .slice()el numero que se le ponga,cortara esa cantidad de caracteres.puede darsele limites con ,
+// con .split() lo mete en un array.si ponemos ""dentro, separara las palabras del array.
+// con .trim() quita espacios al inicio y final de los strings.Va bien para guardar en BBDD.
 console.log(busqueda);
