0085b868dad294156aab67915706b8009d5d1088
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-02 09:26:45 +0200

    funcion con atris y opcionales

--- hola-mundo/JS/19-funciones.js
+++ hola-mundo/JS/19-funciones.js
@@ -3,15 +3,23 @@
 //funciones
 //una funcion es una agrupacion reutilizable de un conjunto de instrucciones.
 
-function calculadora(numero1,numero2){
+function calculadora(numero1,numero2,mostrar = false){
 
+    if(mostrar == false){
     console.log("Suma: "+ (numero1+numero2));
     console.log("Resta: "+(numero1-numero2) );
     console.log("Multiplicacion: "+(numero1*numero2) );
     console.log("Division: "+(numero1/numero2) );
-
+    console.log(mostrar);
+    }
+    else{
+    document.write("Suma: "+ (numero1+numero2)+"<br>");
+    document.write("Resta: "+(numero1-numero2) +"<br>");
+    document.write("Multiplicacion: "+(numero1*numero2)+"<br>" );
+    document.write("Division: "+(numero1/numero2)+"<br>" );
+    }
     return "soy el return";
 
 }
 
-calculadora(12,24);
+calculadora(12,24,true);
