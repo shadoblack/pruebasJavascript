8fd9edc9df81f6896a94e0e755d109038f3c499f
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-03 00:10:43 +0200

    funciones texto

--- hola-mundo/JS/23-funciones-texto.js
+++ hola-mundo/JS/23-funciones-texto.js
@@ -5,28 +5,37 @@ var numero = 444;
 var texto1 = "Hola soy una string";
 var texto2 = "Hola soy la segunda string";
 
-var dato = numero.toString();
-var dato2 = texto1.toUpperCase();
-var dato3 = texto2.toLowerCase();
+// var dato = numero.toString();
+// var dato2 = texto1.toUpperCase();
+// var dato3 = texto2.toLowerCase();
 
 
-console.log(typeof dato);
-console.log( dato2);
-console.log( dato3);
+// console.log(typeof dato);
+// console.log( dato2);
+// console.log( dato3);
 
-//calcular longitud
+// //calcular longitud
 
-var nombre = "Inclopelusia";
+// var nombre = "Inclopelusia";
 
-console.log(nombre.length);
+// console.log(nombre.length);
 
-//concatenar - unir textos
+// //concatenar - unir textos
 
-var textoTotal = texto1 + " " + texto2;
+// var textoTotal = texto1 + " " + texto2;
 
-console.log(textoTotal);
+// console.log(textoTotal);
 
 
-textoTotal = texto1.concat(" "+ texto2);
+// textoTotal = texto1.concat(" "+ texto2);
 
-console.log(textoTotal);
+// console.log(textoTotal);
+
+var busqueda = texto1.lastIndexOf("Hola"); //se puede usar tambien .search
+// con .match hara un array con todas las coincidencias. para que sean varias busquedas,seria /Hola/g sin las ""
+// con .substr,seria .substr(posicion donde inicia,caracteres que saca)
+// con .charAt()el numero que el pongamos sacara la letra en esa posicion.
+// con .startsWith("palabra que buscamos") devolvera si es true o false si tiene el contenido, y siempre al principio del string.
+// con .endsWith() lo mismo pero por donde termina
+// con .includes() devuelve true o false si encuentra o no esto en este string
+console.log(busqueda);
