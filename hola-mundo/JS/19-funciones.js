1f86dc4036fa8e1743b87c61daf1d2f9c1cb6e01
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-02 09:16:07 +0200

    funcion

---
+++ hola-mundo/JS/19-funciones.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//funciones
+//una funcion es una agrupacion reutilizable de un conjunto de instrucciones.
+
+function calculadora(){
+
+    console.log("Hola soy la calculadora");
+    console.log("Si soy yo");
+
+    return "soy el return";
+
+}
+
+calculadora();
