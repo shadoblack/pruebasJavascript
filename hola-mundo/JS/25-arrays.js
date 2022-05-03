3414de6aea2beba9da0630585e2d2c85c44cede9
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-03 09:35:00 +0200

    arrays

--- hola-mundo/JS/25-arrays.js
+++ hola-mundo/JS/25-arrays.js
@@ -13,4 +13,17 @@ console.log(lenguajes);
 console.log(nombres.length);
 
 var elemento =parseInt(prompt("que elemento del array quieres?",0));
-alert(nombres[elemento]);
+if(elemento >= nombres.length){
+
+    alert("introduce el numero correcto menor que "+ nombres.length);
+}else{
+    alert("el usuario seleccionado es " + nombres[elemento]);
+
+}
+
+
+document.write("<h1>lenguajes de programacion del 2020</h1>");
+
+for(var i = 0; i<lenguajes.length; i++){
+    document.write("<li>"+lenguajes[i]+"</li>");
+}
