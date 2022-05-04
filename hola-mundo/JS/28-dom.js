bc1d56d709980dd88b621ddb23fe5f5be67d9353
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-04 13:30:35 +0200

    DOM

---
+++ hola-mundo/JS/28-dom.js
@@ -0,0 +1,40 @@
+'use strict'
+
+//DOM - Document Object Model
+
+function cambiaColor(color){
+    caja.style.color = color;
+}
+//conseguir elementos con un ID concreto
+//var caja =document.getElementById("micaja");
+
+var caja =document.querySelector("#micaja");
+
+caja.innerHTML = "cambio de lo que pone en la caja";
+caja.style.background = "red";
+caja.style.padding = "20px";
+caja.style.color = "white";
+caja.className = "hola";
+
+console.log(caja);
+
+//conseguir elementos por su etiqueta
+
+var todosLosDivs = document.getElementsByTagName('div');
+
+var contenido1= todosLosDivs[2].textContent;
+
+console.log(contenido1);
+
+// todosLosDivs.forEach(valor,indice => {
+    var valor;
+    for(valor in todosLosDivs){
+        if( todosLosDivs[valor].textContent === 'string'){
+    var parrafo = document.createElement("p");
+    var texto = document.createTextNode(todosLosDivs[valor].textContent);
+    parrafo.appendChild(texto);
+    document.querySelector("#miseccion").appendChild(parrafo);
+    }}
+// });
+
+console.log(todosLosDivs);
