8f326afc618f782bc0bd379048e0205800ee1f6a
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-06 08:44:43 +0200

    29-eventos

--- hola-mundo/JS/29-bom.js
+++ hola-mundo/JS/29-bom.js
@@ -5,5 +5,14 @@
 function getBom(){
 console.log(window.innerHeight);
 console.log(window.innerWidth);
-console.log(screen.width)
+console.log(screen.width);
+console.log(window.location);
+}
+
+function redirect(url){
+    window.location.href = url
+}
+
+function abrirVentana(url){
+    window.open(url,"","width=400,heigth=300");
 }
