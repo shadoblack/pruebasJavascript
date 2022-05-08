589a3d6fa21db5ad3b373ec3c7a858abd3937fb6
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-08 08:43:50 +0200

    JSON

---
+++ hola-mundo/JS/33-json.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//JSON - Javascript Object Notation
+
+var pelicula = {
+    titulo: 'Batman vs Superman',
+    year: 2017,
+    pais: 'Estados Unidos'
+}
+
+console.log(pelicula);
+console.log(pelicula.titulo);
+console.log(pelicula.year);
+console.log(pelicula.pais);
+
+pelicula.year = 2022;
+
+console.log(pelicula);
+
+var peliculas = [
+    {titulo: "la verdad duele",year:2016,pais:"francia"},
+    pelicula
+];
+
+console.log(peliculas)
