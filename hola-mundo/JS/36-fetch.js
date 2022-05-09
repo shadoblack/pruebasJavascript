9eaa8dd502b24061c9baf2eab6857b78fa65821e
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-09 09:18:54 +0200

    Fetch

---
+++ hola-mundo/JS/36-fetch.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//fetch(ajax) y peticiones a servicios / API REST
+var div_usuarios = document.querySelector('#usuarios');
+
+var usuarios = [];
+
+fetch('https://reqres.in/api/users')
+.then(data=>data.json())
+.then(users=>{
+    usuarios = users.data;
+    console.log(usuarios);
+
+    usuarios.map((user,i)=>{
+        let nombre = document.createElement('h2');
+
+        nombre.innerHTML = i + "."+ user.first_name + " " + user.last_name;
+
+        div_usuarios.appendChild(nombre);
+
+        document.querySelector(".loading").style.display ='none';
+
+    })
+});
