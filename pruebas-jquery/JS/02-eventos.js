20416afdf0ab478f6f2917038f7d113a203e6ddd
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-13 08:47:08 +0200

    pruebas de hover

--- pruebas-jquery/JS/02-eventos.js
+++ pruebas-jquery/JS/02-eventos.js
@@ -2,13 +2,24 @@ $(document).ready(function () {
   console.log("JQuery OK!!");
 
   //MouseOver y MouseOut
-  var caja = $("#caja");
+  // var caja = $("#caja");
 
-  caja.mouseover(() => {
+  // caja.mouseover(() => {
+  //   $(this).css("background", "red");
+  // });
+
+  // caja.mouseout(() => {
+  //   $(this).css("background", "green");
+  // });
+
+  //hover
+  function cambiaRojo() {
     $(this).css("background", "red");
-  });
+  }
 
-  caja.mouseout(() => {
-    $(this).css("background", "green");
-  });
+  function cambiaVerde() {
+    $(this).css("background", "red");
+  }
+
+  caja.hover(cambiaRojo, cambiaVerde);
 });
