854a2b55b4a70ae9dfbad6532a5b933b227ad327
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-14 14:38:40 +0200

    mas pruebas de AJAX

--- pruebas-jquery/JS/05-ajax.js
+++ pruebas-jquery/JS/05-ajax.js
@@ -1,15 +1,32 @@
-$(document).ready(()=>{
-    console.log("JQuery...OK");
+$(document).ready(() => {
+  console.log("JQuery...OK");
 
-    //load
-    //$("#datos").load("");
+  //load
+  //$("#datos").load("");
 
-    //get y post
-    $.get("https://reqres.in/api/users",{page: 2},function(response){
-        response.data.forEach((element,index)=>{
-            $("#datos").append("<p>"+element.first_name+"</p>");
-        });          
-        
-
+  //get y post
+  $.get("https://reqres.in/api/users", { page: 2 }, function (response) {
+    response.data.forEach((element, index) => {
+      $("#datos").append("<p>" + element.first_name + "</p>");
     });
+  });
+
+  //post
+
+  $("#formulario").submit(function (e) {
+      e.preventDefault();
+    var usuario = {
+      name: $('input[name="name"]').val(),
+      web: $('input[name="web"]').val()
+    };
+
+    console.log(usuario);
+
+    $.post($(this).attr("action"), usuario, function (response) {
+      console.log(response);
+    }).done(()=>{
+        alert("usuario añadido correctamente");
+    });
+    return false;
+  });
 });
