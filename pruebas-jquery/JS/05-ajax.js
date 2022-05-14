d7e8fac1788ee96748de3b8037a0e1880d7efe2c
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-14 14:46:05 +0200

    ajax mas correcto hecho

--- pruebas-jquery/JS/05-ajax.js
+++ pruebas-jquery/JS/05-ajax.js
@@ -22,11 +22,33 @@ $(document).ready(() => {
 
     console.log(usuario);
 
-    $.post($(this).attr("action"), usuario, function (response) {
-      console.log(response);
-    }).done(()=>{
-        alert("usuario añadido correctamente");
+    // $.post($(this).attr("action"), usuario, function (response) {
+    //   console.log(response);
+    // }).done(()=>{
+    //     alert("usuario añadido correctamente");
+    // });
+
+    //otras maneras ajax
+
+    $.ajax({
+        type: 'POST',
+        // dataType: 'json', estos dos dan error y lo considera una accion no permitida 405
+        //  contentType: 'application/json',
+         url: $(this).attr("action"),
+        data: usuario,
+        beforeSend: ()=>{
+            console.log("enviando usuario...");
+        },
+        success: (response)=>{
+            console.log(response);
+        },
+        error:  ()=>{
+            console.log("ha ocurrido un error");
+        },
+        timeout: 2000
     });
+
+
     return false;
   });
 });
