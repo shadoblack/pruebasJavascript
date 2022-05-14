86401ff672afa82ff6c27ff979c0f161e6d3e5d2
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-14 13:44:19 +0200

    efectos jquery(una pasada2)

--- pruebas-jquery/JS/04-efectos.js
+++ pruebas-jquery/JS/04-efectos.js
@@ -1,25 +1,37 @@
-$(document).ready(()=>{
-    console.log("JQuery...OK");
+$(document).ready(() => {
+  console.log("JQuery...OK");
 
-    $("#caja").hide();
+  $("#caja").hide();
 
-    $("#mostrar").click(()=>{
-        $(this).hide();
-        $("#ocultar").show();
-        $("#caja").fadeIn('slow');
+  $("#mostrar").click(() => {
+    $(this).hide();
+    $("#ocultar").show();
+    $("#caja").fadeIn("slow");
+  });
+  $("#ocultar").click(() => {
+    $(this).hide();
+    $("#mostrar").show();
+    $("#caja").fadeOut("slow");
+  });
 
-    });
-    $("#ocultar").click(()=>{
-        $(this).hide();
-        $("#mostrar").show();
-        $("#caja").fadeOut('slow');
-        
+  $("#todoEnUno").click(() => {
+    $("#caja").fadeToggle("fast");
 
-    });
+    //fadetoggle,slidetoggle,slideDown,SlideUp...hay muchos mas.ir probando
+  });
 
-    $("#todoEnUno").click(()=>{
-        $("#caja").fadeToggle('fast');
-
-        //fadetoggle,slidetoggle,slideDown,SlideUp...hay muchos mas.ir probando
-    })
+  $("#animame").click(() => {
+    $("#caja")
+      .animate(
+        { marginLeft: "500px", fontSize: "40px", height: "120px" },
+        "slow"
+      )
+      .animate(
+        {
+          borderRadius: "900px",
+          marginleft: "0px",
+        },
+        "slow"
+      );
+  });
 });
