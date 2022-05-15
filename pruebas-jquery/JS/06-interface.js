0cba9af72642ee23725e52eecd3c77e614e7345c
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-15 13:47:19 +0200

    jqueryui2

--- pruebas-jquery/JS/06-interface.js
+++ pruebas-jquery/JS/06-interface.js
@@ -7,4 +7,22 @@ $(document).ready(()=>{
 
     $('.elemento').resizable();
 
+    //seleccionar y ordenar elementos
+
+    // $('.lista-seleccionable').selectable();
+    $('.lista-seleccionable').sortable({
+        update: function(event,ui){
+            console.log("ha cambiado la lista");
+        }
+    });
+
+    //drop
+    $('#elemento-movido').draggable();
+    $('#area').droppable({
+        drop: function(){
+            console.log("has soltado algo dentro del area");
+        }
+    });
+    
+
 });
